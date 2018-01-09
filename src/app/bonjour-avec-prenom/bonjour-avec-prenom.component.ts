05ad083cbe5aaf05fa63d9fa96147eb3a6b096e2
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2018-01-09 12:13:44 +0100

    test angular

---
+++ src/app/bonjour-avec-prenom/bonjour-avec-prenom.component.ts
@@ -0,0 +1,21 @@
+import {
+    Component,
+    OnInit,
+    ViewEncapsulation,
+    Input // ajouter l'import du composant Input
+} from '@angular/core';
+@Component({
+  selector: 'app-bonjour-avec-prenom',
+  templateUrl: './bonjour-avec-prenom.component.html',
+  styleUrls: ['./bonjour-avec-prenom.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BonjourAvecPrenomComponent implements OnInit {
+  
+  @Input() prenom:string; // déclarer que ce champ est un paramètre du composant
+  constructor() {
+  // ici plus de valorisation de la propriété du composant
+  }
+  ngOnInit() {
+  }
+}
